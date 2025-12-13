ae597fce2486e20e1e8f176bb5b9f17115d76334
Kieran Mason <me@kieranmason.co.uk>
2025-12-13 00:34:45 +0000

    Add Y2025 D11

---
+++ Y2025/D11/DirectedGraph.cs
@@ -0,0 +1,32 @@
+﻿namespace AoC.Y2025.D11;
+
+public class DirectedGraph<T> where T : IEquatable<T>
+{
+    private readonly Dictionary<T, HashSet<T>> _in = new();
+    private readonly Dictionary<T, HashSet<T>> _out = new();
+
+    public IEnumerable<T> In(T node) => GetOrEmpty(node, _in);
+    public IEnumerable<T> Out(T node) => GetOrEmpty(node, _out);
+
+    private static HashSet<T> GetOrEmpty(T node, Dictionary<T, HashSet<T>> direction)
+    {
+        if (direction.TryGetValue(node, out var set) == false)
+        {
+            return direction[node] = [];
+        }
+
+        return set;
+    }
+
+    public void Connect(T from, T to)
+    {
+        GetOrEmpty(to, _in).Add(from);
+        GetOrEmpty(from, _out).Add(to);
+    }
+
+    public void Disconnect(T from, T to)
+    {
+        GetOrEmpty(to, _in).Remove(from);
+        GetOrEmpty(from, _out).Remove(to);
+    }
+}
---
+++ Y2025/D11/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace AoC.Y2025.D11;
+
+public class Solution : SolutionBase
+{
+    public override void Run()
+    {
+        var graph = ParseInput(ReadInputAsLines());
+        var memo = new Dictionary<Traversal, long>();
+
+        var direct = Paths("you", "out");
+        Log(direct);
+
+        var stops = Paths("svr", "dac") * Paths("dac", "fft") * Paths("fft", "out") +
+                    Paths("svr", "fft") * Paths("fft", "dac") * Paths("dac", "out");
+        Log(stops);
+
+        return;
+
+        long Paths(string from, string to) => CountPaths(graph, new Traversal(from, to), memo);
+    }
+
+    private static long CountPaths(DirectedGraph<string> graph, Traversal traversal, Dictionary<Traversal, long> memo)
+    {
+        if (memo.TryGetValue(traversal, out var cached))
+            return cached;
+
+        if (traversal.Start == traversal.End)
+            return memo[traversal] = 1L;
+
+        return memo[traversal] = graph.Out(traversal.Start)
+            .Sum(connected => CountPaths(graph, traversal with { Start = connected }, memo));
+    }
+
+    private static DirectedGraph<string> ParseInput(string[] input)
+    {
+        var graph = new DirectedGraph<string>();
+        foreach (var line in input)
+        {
+            var split = line.Split(": ");
+            var from = split[0];
+            var destinations = split[1].Split(' ');
+
+            foreach (var destination in destinations)
+            {
+                graph.Connect(from, destination);
+            }
+        }
+
+        return graph;
+    }
+
+    private readonly record struct Traversal(string Start, string End);
+}
